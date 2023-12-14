1a1ca0d132f605da309fad0c806cdab61b47349e
ttolkynb <workfortime37@gmail.com>
2023-12-15 02:50:00 +0600

    add button

---
+++ .dart_tool/flutter_build/6a1278b6ca61461eb2208c1d60b4f75b/native_assets.d
@@ -0,0 +1,1 @@
+ /Users/apple/magic-8-ball-flutter/.dart_tool/flutter_build/6a1278b6ca61461eb2208c1d60b4f75b/native_assets.yaml: 
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade900,
+          title: Text('Ask Me Anything',
+            style: TextStyle(
+              color: Colors.white
+            ),
+          ),
+        ),
+        body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballINumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballINumber = Random().nextInt(5)+1;
+          });
+          print('I got clicked');
+        },
+        child: Image.asset('images/ball$ballINumber.png'),
+      ),
+    );
+  }
+}
